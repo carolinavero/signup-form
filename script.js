590609944b0c222cac6a508da231aeb87c53bc78
Carolina Vero <carolina@w42.com.br>
2020-03-25 17:08:42 -0300

    validate form javascript

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var formEl = document.querySelector('form')
 var submitBtn = document.querySelector('button[type="submit"]')
 var inputEl = document.querySelectorAll('input')
 var errorMsgEl = document.querySelectorAll('.error-msg')
-var inputText = ''
 
 var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
@@ -12,36 +11,48 @@ formEl.addEventListener('submit', e => {
     console.log("evento", e);
 
     inputEl.forEach(input => {
+
+        var spanError = document.createElement('span')
+        var placeholderValue = input.placeholder;
+        spanError.textContent = `${placeholderValue} cannot be empty`
         
         /* empty */
-        if (input.value.length == 0) {
-            input.classList.add("error")
-
-            var spanError = document.createElement('span')
-            var placeholderValue = input.placeholder;
-            spanError.textContent = `${placeholderValue} cannot be empty`
+        if (input.value.length == 0 && !input.classList.contains("error"))  {
+            console.log("empty e sem error")  
             spanError.setAttribute("class", "error-msg")
-
             input.parentNode.insertBefore(spanError, input.nextSibling);
-            
+            input.classList.add("error")
         }
-        /* not empty, invalid email */
-        else if (input.value.match(mailFormat)) {
-            console.log("email invalido")
-            var spanErrorEmail = document.createElement('span')
-            spanErrorEmail.textContent = 'Looks like this is not an email'
-            console.log(spanErrorEmail)
-            
-        }
+       
         /* not empty */
-        else {
-
-            input.classList.remove("error")
-            /* spanError.classList.add("hidden") */
-        }
+        else if (input.value.length > 0) {
         
+            if (input.classList.contains("error")) {
+                console.log("not empty")
+                input.classList.remove("error")
+                console.log("span error", spanError)
+                input.nextSibling.remove(spanError)
+            }
+
+            /* not empty, invalid email */
+            if (input.classList.contains("email-field")) {
+                console.log("campo de email")
+
+                if (!input.value.match(mailFormat)) {
+                    console.log("email invalido")
+                    var spanErrorEmail = document.createElement('span')
+                    spanErrorEmail.textContent = 'Looks like this is not an email'
+                    console.log(spanErrorEmail)
+                    spanErrorEmail.setAttribute("class", "error-msg")
+                    input.parentNode.insertBefore(spanErrorEmail, input.nextSibling);
+                    input.classList.add("error");
+                }
+            }
+            
+
+        } 
+
     })
-   
     
 })
 
